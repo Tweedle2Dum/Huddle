709d3ffc81dd9d95f1290b35d340452742039cc4
Lioncat2002 <sankhayan2002@gmail.com>
2022-11-12 00:40:14 +0530

    feat: initialized the backend

---
+++ backend/HuddleBackend/src/main/java/com/huddle/huddlebackend/HuddleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.huddle.huddlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuddleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuddleBackendApplication.class, args);
+    }
+
+}
